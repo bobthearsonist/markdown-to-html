4dec46927af5e8d84cbf4897c2925690de16755f
Martin Perry <mperr01@gmail.com>
2023-03-12 21:37:28 -0400

    add initial red tests

--- markdown-to-html.tests/UnitTest1.cs
+++ markdown-to-html.tests/UnitTest1.cs
@@ -1,4 +1,9 @@
+using System.Reflection;
+using FluentAssertions;
+
 namespace markdown_to_html.tests;
+
+using markdown_to_html_lib;
 
 public class Tests
 {
@@ -8,8 +13,20 @@ public class Tests
     }
 
     [Test]
-    public void Test1()
+    public void Example1()
     {
-        Assert.Pass();
+        var input = new MemoryStream();
+        var expected = new MemoryStream();
+        
+        Assembly.GetExecutingAssembly()
+                .GetManifestResourceStream("markdown_to_html.tests.example.markdown.example1.md")
+                ?.CopyTo(input);
+        Assembly.GetExecutingAssembly()
+            .GetManifestResourceStream("markdown_to_html.tests.example.html.example1.html")
+            ?.CopyTo(expected);
+
+        var actual = new MemoryStream();
+        MarkDownToHtml.Convert(input).CopyTo(actual);
+        actual.ToArray().Should().BeEquivalentTo(expected.ToArray());
     }
 }
