4cdd3ba636abf9970d07736d20866bfe1bddfabf
Martin Perry <mperr01@gmail.com>
2023-03-12 21:36:59 -0400

    move implementation to lib

---
+++ markdown-to-html-lib/MarkDownToHtml.cs
@@ -0,0 +1,10 @@
+﻿using Markdig
+
+namespace markdown_to_html_lib;
+public static class MarkDownToHtml
+{
+    public static Stream Convert(Stream input)
+    {
+        return new MemoryStream(System.Text.Encoding.ASCII.GetBytes("nope"));
+    }
+}
