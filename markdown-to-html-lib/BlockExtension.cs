ddbac0b201b09d80eacc7325534678148f22b148
Martin Perry <mperr01@gmail.com>
2023-03-19 17:05:33 -0500

    add lookup table

--- markdown-to-html-lib/BlockExtension.cs
+++ markdown-to-html-lib/BlockExtension.cs
@@ -6,6 +6,16 @@ namespace markdown_to_html_lib;
 
 public static class BlockExtension
 {
+    //use values that are function pointers and return the parsed/formatted html? Func<HtmlNode>()
+    static Dictionary<Type, string> blockTypeToHtmlTagLookup = new()
+    {
+        // TODO you will need to pars what level heading it is somehow and then do the other levels
+        { typeof(HeadingBlock), "h1" },
+        // TODO find what these types are
+        // { typeof("Text"),"p"},
+        // { typeof("Link"),"a"},
+        // { typeof("Blank"),""}
+    };
 
     public static HtmlNode Map(this Block block)
     {
