2ebdb0ca9acca30a342a145c012e32f92f7f7f23
Martin Perry <mperr01@gmail.com>
2023-03-19 10:42:41 -0500

    grab the block value

--- markdown-to-html-lib/BlockExtension.cs
+++ markdown-to-html-lib/BlockExtension.cs
@@ -1,5 +1,6 @@
 using HtmlAgilityPack;
 using Markdig.Syntax;
+using Markdig.Syntax.Inlines;
 
 namespace markdown_to_html_lib;
 
@@ -9,7 +10,10 @@ public static class BlockExtension
     public static HtmlNode Map(this Block block)
     {
         //TODO implement the actual mappings.
-        var node = HtmlNode.CreateNode("<h1>" + block + "</h1>");
+        var node = HtmlNode.CreateNode("<h1>" 
+                                       + (block as HeadingBlock)?.Inline!.First() 
+                                       + "</h1>"
+                                );
         return node;
     }
 }
