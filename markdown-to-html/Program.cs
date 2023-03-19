8ce3fcbfc87108fcf3936445e9c41fb0333dd567
Martin Perry <mperr01@gmail.com>
2023-03-19 10:30:24 -0500

    add outline for cli

--- markdown-to-html/Program.cs
+++ markdown-to-html/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+// TODO take some md as a string directly
+// default mode
+
+// TODO take a file as md and output a file as html
+// --input file --output file
